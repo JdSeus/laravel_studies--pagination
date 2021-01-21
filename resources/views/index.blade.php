cf0012e87f6510bdc4a7059f6c54e4a443d7a808
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-21 15:24:20 -0300

    criando tabela

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,53 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{csrf_token()}}">
+        <link href="{{asset('css/app.css')}}" rel="stylesheet">
         <title>Paginação</title>
+        <style>
+            body {
+                padding: 20px;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
-            <h1>a</h1>
+            <div class="card text-center">
+                <div class="card-header">Tabela de cliente</div>
+                <div class="card-body">
+                    <h5 class="card-title">Exibindo 10 clientes de 1000 (1 a 10)</h5>
+                    <table class="table table-hover">
+                        <thead>
+                            <th scope="col">#</th>
+                            <th scope="col">Nome</th>
+                            <th scope="col">Sobrenome</th>
+                            <th scope="col">E-mail</th>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Cesar</td>
+                                <td>Augusto</td>
+                                <td>cesar@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Cesar</td>
+                                <td>Augusto</td>
+                                <td>cesar@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>1</td>
+                                <td>Cesar</td>
+                                <td>Augusto</td>
+                                <td>cesar@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-footer">
+                    Paginas
+                </div>
+            </div>
         </div>
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
     </body>
