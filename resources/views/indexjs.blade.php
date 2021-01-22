963864334d529e940560966109a5f5f27977097f
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-22 09:27:33 -0300

    page js carregando clientes

--- resources/views/indexjs.blade.php
+++ resources/views/indexjs.blade.php
@@ -17,7 +17,7 @@
             <div class="card text-center">
                 <div class="card-header">Tabela de cliente</div>
                 <div class="card-body">
-                    <table class="table table-hover">
+                    <table class="table table-hover" id="tabelaClientes">
                         <thead>
                             <th scope="col">#</th>
                             <th scope="col">Nome</th>
@@ -40,5 +40,36 @@
         </div>
         
         <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    
+        <script type="text/javascript">
+
+            function montarLinha(cliente) {
+                return '<tr> ' +
+                    '<td>' + cliente.id +'</td>' +
+                    '<td>' + cliente.nome +'</td>' +
+                    '<td>' + cliente.sobrenome +'</td>' +
+                    '<td>' + cliente.email +'</td>' +
+                '</tr>';
+            }
+
+            function montarTabela(data) {
+                $("#tabelaClientes>tbody>tr").remove();
+                for (i=0; i<data.data.length; i++) {
+                    s = montarLinha(data.data[i]);
+                    $("#tabelaClientes>tbody").append(s);
+                }
+            }
+
+            function carregarClientes(pagina) {
+                $.get( '/json', {page: pagina}, function(resp){
+                    console.log(resp);
+                    montarTabela(resp);
+                });
+            }
+
+            $(function() {
+                carregarClientes(1);
+            });
+        </script>
     </body>
 </html>
